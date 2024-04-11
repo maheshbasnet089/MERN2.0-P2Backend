08b2e44b45e8fc794b5cd78002a0e07239717631
maheshbasnet089 <basnetmanish089@gmail.com>
2024-04-11 20:53:22 +0545

    category crud

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,4 +1,5 @@
 import Category from "../database/models/Category"
+import {Request,Response} from 'express'
 
 
 class CategoryController{
@@ -21,6 +22,65 @@ class CategoryController{
         }else{
             console.log("Categories already seeded")
         }
+    }
+
+    async addCategory(req:Request,res:Response):Promise<void>{
+        const {categoryName} = req.body 
+        if(!categoryName) {
+            res.status(400).json({message : "Please provide categoryName"})
+            return
+        } 
+
+        await Category.create({
+            categoryName
+        })
+        res.status(200).json({
+            message : "Category Added successfully"
+        })
+
+    }
+
+    async getCategories(req:Request,res:Response):Promise<void>{
+        const data = Category.findAll()
+        res.status(200).json({
+            message : "Categories fetched",
+            data
+        })
+    }
+
+    async deleteCategory(req:Request,res:Response){
+        const {id} = req.params 
+        const data = await Category.findAll({
+            where : {
+                id
+            }
+        })
+        if(data.length === 0){
+            res.status(404).json({
+                message : "No category with that id"
+            })
+        }else{
+            await Category.destroy({
+                where : {
+                    id
+                }
+            })
+            res.status(200).json({
+                message : "Category deleted"
+            })
+        }
+    }
+    async updateCategory(req:Request,res:Response):Promise<void>{
+        const {id} = req.params 
+        const {categoryName} = req.body
+        await Category.update({categoryName},{
+            where : {
+                id
+            }
+        })
+        res.status(200).json({
+            message : "Category updated"
+        })
     }
 }
 
