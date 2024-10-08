3ad6c68f1e8e7b300839e612105941334d8e0277
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-08 19:46:16 +0545

    finish

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,10 @@ import categoryController from './controllers/categoryController'
 import cartRoute from './routes/cartRoute'
 import orderRoute from './routes/orderRoute'
 import cors from 'cors'
+import { Server } from 'socket.io'
+import { promisify } from 'util'
+import jwt from 'jsonwebtoken'
+import User from './database/models/User'
 
 app.use(cors({
     origin : '*'
@@ -32,9 +36,42 @@ app.use("/admin/category",categoryRoute)
 app.use("/customer/cart",cartRoute)
 app.use("/order",orderRoute)
 
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     categoryController.seedCategory()
     console.log("Server has started at port ", PORT)
+})
+const io = new Server(server,{
+    cors : {
+        origin : ['http://localhost:5173','http://localhost:5174']
+    }
+})
+
+let onlineUsers:any = []
+const addToOnlineUsers = (socketId:string,userId:string,role:string)=>{
+    onlineUsers = onlineUsers.filter((user:any)=>user.userId !==userId)
+    onlineUsers.push({socketId,userId,role})
+}
+io.on("connection",async (socket)=>{
+    console.log("A client connected")
+    const {token} = socket.handshake.auth 
+    if(token){
+        //@ts-ignore
+        const decoded = await promisify(jwt.verify)(token,process.env.SECRET_KEY)
+        //@ts-ignore
+        const doesUserExists = await User.findByPk(decoded.id)
+        if(doesUserExists){
+            addToOnlineUsers(socket.id,doesUserExists.id,doesUserExists.role)
+        }
+    }
+    socket.on("updatedOrderStatus",({status,orderId,userId})=>{
+        const findUser = onlineUsers.find((user:any)=>user.userId == userId)
+        if(findUser){
+            io.to(findUser.socketId).emit("statusUpdated",
+                {status,orderId}
+            )
+        }
+    })
+    console.log(onlineUsers)
 })
 
 // sudo /Applications/XAMPP/xamppfiles/xampp start
